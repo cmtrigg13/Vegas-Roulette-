40a792344d4abcf8742e14ca8dc8efe104b45274
cmtrigg13 <cmtrigg@gmail.com>
2026-05-09 11:41:12 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
-const CACHE = 'vegas-roulette-v1';
+const CACHE = 'vegas-roulette-v2';
 const ASSETS = ['/', '/index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
